46b89c3b44e62f8901746ebfbfd5ce3dd2a68ad8
mine3873 <94094712+mine3873@users.noreply.github.com>
2024-08-29 20:01:06 +0900

    Title: 중앙값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240829/중앙값 구하기/find-the-median.cpp
+++ 240829/중앙값 구하기/find-the-median.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
-    cout << (a < b ? (b < c ? b : c) : (a < c ? a : c));
+    cout << (a < b ? (b < c ? b : c) : (a < c ? a : (b < c ? c : b)));
     return 0;
 }
