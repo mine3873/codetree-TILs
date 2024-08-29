0308548511b70a0230ccfe348e152e1bec85bd45
mine3873 <94094712+mine3873@users.noreply.github.com>
2024-08-29 19:49:55 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240829/3 또는 5의 배수/multiples-of-3-or-5.cpp
+++ 240829/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-    cout << (a%3?"No" : "YES") << '\n' << (a%5?"NO" : "YES");
+    cout << (a%3?"NO" : "YES") << '\n' << (a%5?"NO" : "YES");
     return 0;
 }
